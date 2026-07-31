f961ecaad8e024845d74ced74159e335a959c0ef
Iamollie1482 <pang104@gmail.com>
2026-07-31 09:39:51 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,16 +1,29 @@
-const CACHE = 'cat-health-v1';
+const CACHE = 'cat-health-v3';
 const FILES = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
   self.skipWaiting();
 });
+
 self.addEventListener('activate', e => {
   e.waitUntil(caches.keys().then(keys =>
     Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
   ));
   self.clients.claim();
 });
+
 self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+  // 永遠先嘗試網路取得最新版，失敗才用快取
+  e.respondWith(
+    fetch(e.request).then(function(response) {
+      var clone = response.clone();
+      caches.open(CACHE).then(function(cache) {
+        cache.put(e.request, clone);
+      });
+      return response;
+    }).catch(function() {
+      return caches.match(e.request);
+    })
+  );
 });
